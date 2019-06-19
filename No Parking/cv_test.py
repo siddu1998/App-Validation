bbcf400742e3dec93e28de4e623bde974c0600d7
Sai Siddartha Maram <smaram_be16@thapar.edu>
2019-06-19 14:36:28 -0400

    Annotations

---
+++ No Parking/cv_test.py
@@ -0,0 +1,4 @@
+import cv2
+img=cv2.imread('103211.jpg')
+cv2.imshow('img',img)
+cv2.waitKey(0)
---
+++ No Parking/log.py
@@ -0,0 +1,12 @@
+import cv2
+from core_calculations import *
+import utm
+
+
+
+calculation_of_distances('0002024.jpg','0002025.jpg','sign_annotations.csv','camera_cordinates.csv')
+calculation_of_distances('0002025.jpg','0002026.jpg','sign_annotations.csv','camera_cordinates.csv')
+calculation_of_distances('0002026.jpg','0002027.jpg','sign_annotations.csv','camera_cordinates.csv')
+calculation_of_distances('0002027.jpg','0002028.jpg','sign_annotations.csv','camera_cordinates.csv')
+calculation_of_distances('0002028.jpg','0002029.jpg','sign_annotations.csv','camera_cordinates.csv')
+
